6679d639d3d7991c2b3dc3e932232825a62fe36e
keithchasen <keithchasen89@gmail.com>
2019-05-23 17:53:18 +0300

    timer components

---
+++ src/components/EditableTimerList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class EditableTimerList extends Component {
+    render() {
+        return(
+            <div></div>
+        )
+    }
+}
+
+export default EditableTimerList
---
+++ src/components/ToggleableTimerForm.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class ToggleableTimerForm extends Component {
+    render() {
+        return(
+            <div></div>
+        )
+    }
+}
+
+export default ToggleableTimerForm
