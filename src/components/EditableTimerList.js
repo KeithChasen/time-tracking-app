e2c1eb00a8216ba48dbe5c14c71b1857d4c817a1
keithchasen <keithchasen89@gmail.com>
2019-05-23 22:09:55 +0300

    timer components

--- src/components/EditableTimerList.js
+++ src/components/EditableTimerList.js
@@ -5,7 +5,20 @@ class EditableTimerList extends Component {
     render() {
         return(
             <div id='timers'>
-                <EditableTimer />
+                <EditableTimer
+                    title='React'
+                    project='Education'
+                    elapsed='8986300'
+                    runningSince={null}
+                    editFormOpen={false}
+                />
+                <EditableTimer
+                    title='Redux'
+                    project='Education'
+                    elapsed='3986300'
+                    runningSince={null}
+                    editFormOpen={true}
+                />
             </div>
         )
     }
