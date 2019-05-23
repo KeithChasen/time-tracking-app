20ffdfa72530e22a0895e65dcb143867342bc4cc
keithchasen <keithchasen89@gmail.com>
2019-05-23 17:38:08 +0300

    TimersDashboard component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import TimersDashboard from "./components/TimersDashboard";
 
 
 function App() {
   return (
     <div className="App">
+      <TimersDashboard />
     </div>
   );
 }
---
+++ src/components/TimersDashboard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class TimersDashboard extends Component {
+    render() {
+        return(
+            <div>
+                TimersDashboard
+            </div>
+        )
+    }
+}
+
+export default TimersDashboard
