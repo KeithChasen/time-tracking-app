3f753a94e8430e321ce57fb4f1c16f2886b6b978
keithchasen <keithchasen89@gmail.com>
2019-06-11 22:56:13 +0300

    passing timers through props to EditableTimerList inside TimersDashboard

--- src/components/EditableTimerList.js
+++ src/components/EditableTimerList.js
@@ -3,22 +3,20 @@ import EditableTimer from "./EditableTimer";
 
 class EditableTimerList extends Component {
     render() {
+        const timers = this.props.timers.map(timer => (
+            <EditableTimer
+                key={timer.id}
+                id={timer.id}
+                title={timer.title}
+                project={timer.project}
+                elapsed={timer.elapsed}
+                runningSince={timer.runningSince}
+            />
+        ))
+
         return(
             <div id='timers'>
-                <EditableTimer
-                    title='React'
-                    project='Education'
-                    elapsed='8986300'
-                    runningSince={null}
-                    editFormOpen={false}
-                />
-                <EditableTimer
-                    title='Redux'
-                    project='Education'
-                    elapsed='3986300'
-                    runningSince={null}
-                    editFormOpen={true}
-                />
+                {timers}
             </div>
         )
     }
--- src/components/TimersDashboard.js
+++ src/components/TimersDashboard.js
@@ -3,11 +3,32 @@ import EditableTimerList from "./EditableTimerList";
 import ToggleableTimerForm from "./ToggleableTimerForm";
 
 class TimersDashboard extends Component {
+    state = {
+        timers: [
+            {
+                title: 'Practice squat',
+                project: 'Gym Chores',
+                id: 123,
+                elapsed: 5456099,
+                runningSince: Date.now()
+            },
+            {
+                title: 'Bake squash',
+                project: 'Kitchen Chores',
+                id: 1235,
+                elapsed: 1273998,
+                runningSince: null
+            }
+        ]
+    }
+
     render() {
         return(
             <div className="ui three column centered grid">
                 <div className="column">
-                    <EditableTimerList />
+                    <EditableTimerList
+                        timers={this.state.timers}
+                    />
                     <ToggleableTimerForm isOpen={false} />
                 </div>
             </div>
